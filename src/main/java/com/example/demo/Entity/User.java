ee69695ea6f08a3ae79ad9d3f711e5cc496b2797
ひな <nishi.hina29@gmail.com>
2026-04-01 12:29:32 +0900

    firstcommit

---
+++ src/main/java/com/example/demo/Entity/User.java
@@ -0,0 +1,10 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="second_spring_todo")
+public class User {
+
+}
---
+++ src/main/java/com/example/demo/SecondSpringTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSpringTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondSpringTodoAppApplication.class, args);
+	}
+
+}
